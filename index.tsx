ce9c5aef32204c0b49c751f5300cd112204b5a42
sashagraves <all-exa@ya.ru>
2020-03-16 16:42:17 +0300

    RZR-1: ESlint rules, index.tsx is appropriate

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react'
-import { Component } from 'react'
-import './index.scss'
-import ReactDOM from 'react-dom';
+/* eslint react/destructuring-assignment: "off" */
 
+import * as React from 'react';
+import './index.scss';
+import ReactDOM from 'react-dom';
 
 
 interface HelloProps {
@@ -10,47 +10,42 @@ interface HelloProps {
   framework: string;
 }
 
-class HelloWorld extends Component<HelloProps, {}> {
-  
-  render() {
-    return (<h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>)
-}
+function HelloWorld(props: HelloProps) {
+  return (
+    <h1>
+      Hello from {props.compiler} and {props.framework}!
+    </h1>
+  );
 }
 
 function Button(props: any) {
-    const handleClick = () => props.onClickFunction(props.increment);
-      return (
-        <button onClick={handleClick}>
-        +{props.increment}
-      </button>
-    );
-  }
+  const handleClick = () => props.onClickFunction(props.increment);
+  return (
+    <button type="button" onClick={handleClick}>
+      +
+      {props.increment}
+    </button>
+  );
+}
   
-  const Display = (props: any) : JSX.Element => {
-        return ( 
-          <div className="result">{props.message}</div>
-        )
-  }
+const Display = (props: any) : JSX.Element => (
+  <div className="result">{props.message}</div>
+);
   
-  function App() {
-      const [counter, setCounter] = React.useState(0);
-      const incrementCounter = (incrementValue: number) => setCounter(counter+incrementValue);
-
-    return (
-      <div>
-        <HelloWorld compiler="TypeScript" framework="React" />
-        <Button onClickFunction={incrementCounter} increment={1} />
-        <Button onClickFunction={incrementCounter} increment={5} />
-        <Button onClickFunction={incrementCounter} increment={10} />
-        <Button onClickFunction={incrementCounter} increment={100} />
-        <Display message={counter} />
-      </div>  
-    );
-  }
-
+function App() {
+  const [counter, setCounter] = React.useState(0);
+  const incrementCounter = (incrementValue: number) => setCounter(counter + incrementValue);
+  return (
+    <div>
+      <HelloWorld compiler="TypeScript" framework="React" />
+      <Button onClickFunction={incrementCounter} increment={1} />
+      <Button onClickFunction={incrementCounter} increment={5} />
+      <Button onClickFunction={incrementCounter} increment={10} />
+      <Button onClickFunction={incrementCounter} increment={100} />
+      <Display message={counter} />
+    </div>
+  );
+}
 
 
-
-
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
