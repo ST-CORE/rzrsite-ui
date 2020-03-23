604e01948208b5d5aa5920a528a2d7955415d3cb
sashagraves <all-exa@ya.ru>
2020-03-23 13:43:17 +0300

    RZR-13: Modals

--- source/shared/modal/modal.tsx
+++ source/shared/modal/modal.tsx
@@ -1,34 +1,36 @@
 import * as React from 'react';
+import Button from '../button/button';
+import './modal.scss';
+
 
 interface ModalProps {
-  children: JSX.Element,
-  showModal: number
+  buttonLabel: string,
+  buttonClassName?: string,
+  children: JSX.Element
 }
 
-export default ({ children, showModal }: ModalProps) : (JSX.Element | null) => {
-  const [show, setShow] = React.useState(showModal);
-  console.log(`before handle ${show}`);
+export default ({ buttonLabel, buttonClassName, children }: ModalProps) => {
+  const [showModal, setState] = React.useState(false);
+  const clickHandler = () => setState(!showModal);
   
-  
-  const clickHandler = () => {
-    console.log(`after handle ${show}`);
-    setShow(90);
-  };
-    
-  let result: (JSX.Element | null);
-  if (show) {
-    result = (
-      <div className="modal">
-        <button type="button" className="close" aria-label="Close" onClick={clickHandler}>
-          <span aria-hidden="true">&times;</span>
-        </button>
-                
-        <div className="modal-content">
-          {children}
+  if (showModal) {
+    return (
+      <div>
+        <div className="modal">
+          <button type="button" className="close" aria-label="Close" onClick={clickHandler}>
+            <span aria-hidden="true">&times;</span>
+          </button>
+                  
+          <div className="modal-content">
+            {children}
+          </div>
         </div>
-        
+        <Button label={buttonLabel} buttonClassName={buttonClassName} clickHandler={clickHandler} />
       </div>
     );
-  } else result = null;
-  return result;
+  }
+  
+  return (
+    <Button label={buttonLabel} buttonClassName={buttonClassName} clickHandler={clickHandler} />
+  );
 };
