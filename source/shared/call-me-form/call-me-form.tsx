e4802b64e1cf568c6c7add7ba61acbac0942b121
sashagraves <all-exa@ya.ru>
2020-03-24 17:14:45 +0300

    RZR-13: some styles for Form-popup

--- source/shared/call-me-form/call-me-form.tsx
+++ source/shared/call-me-form/call-me-form.tsx
@@ -1,23 +1,25 @@
 import * as React from 'react';
 import './call-me-form.scss';
+import Button from '../button/button';
+import * as V from '../../variables';
 
 export default () => (
   <div className="call-me-form" tabIndex={-1} role="dialog">
 
     <form action="" method="" name="callMeForm">
       <fieldset>
-        <legend>Как с вами связаться?</legend>
-        <label htmlFor="name">
-          Имя:
-          <input type="text" id="name" name="name" required />
-        </label>
-        <label htmlFor="tel">
-          Телефон:
-          <input type="tel" id="tel" name="tel" required />
-        </label>
-        <button type="button">
-          Позвоните мне
-        </button>
+        <legend>
+          Вам позвонить?
+        </legend>
+        <span>Оставьте своё имя и номер, и мы сами вам позвоним.</span>
+        <div className="center">
+          <label htmlFor="name"> Имя: </label>
+          <input type="text" id="name" name="name" placeholder="Имя" />
+          <label htmlFor="tel">Телефон:</label>
+          <input type="tel" id="tel" name="tel" required placeholder="+7-999-000-00-00" />
+          <Button label="Позвоните мне!" buttonClassName={V.longCallmeButton} clickHandler="" />
+          
+        </div>
       </fieldset>
     </form>
   </div>
