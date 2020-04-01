1ee187d700d80a0b9916d3baa065ca0a0ce81ad9
sashagraves <all-exa@ya.ru>
2020-04-01 15:21:32 +0300

    RZR-28 - after review

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
-import RouteSwitcher from './components/route-switcher';
+import PageRouting from './components/page-routing';
 
 export default () => (
   <Router>
     <Header lightTheme />
-    <RouteSwitcher />
+    <PageRouting />
     <Footer />
   </Router>
 );
