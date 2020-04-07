6621ffab796d0cde4f6571ba79a129e8b85846f4
sashagraves <all-exa@ya.ru>
2020-04-07 15:30:37 +0300

    RZR-38: Header now changes its style while switching to another page. Also: reorganized App and index.tsx and main scss files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import PageRouting from './components/page-routing/page-routing';
 
-export default () => (
-  <Router>
-    <Header lightTheme />
-    <main className="main-content">
-      <PageRouting />
-    </main>
-    <Footer />
-  </Router>
-);
+export default () => {
+  const location = useLocation();
+  let theme: boolean = false;
+  if (location.pathname === '/') {
+    theme = true;
+  }
+  
+  return (
+    <div>
+      <Header lightTheme={theme} />
+      <main className="main-content">
+        <PageRouting />
+      </main>
+      <Footer />
+    </div>
+  );
+};
