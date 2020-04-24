b5131118ff329803253a8444973944728baa538e
sashagraves <all-exa@ya.ru>
2020-04-25 00:36:18 +0300

    RZR-75: removed "container-small" class to first div

--- src/components/home/landing-info/landing-info.tsx
+++ src/components/home/landing-info/landing-info.tsx
@@ -2,21 +2,19 @@ import * as React from 'react';
 import './landing-info.scss';
 
 export default () => (
-  <div className="landing-info">
-    <div className="container-small">
-      <div className="why-us">
-        <h2>Почему стоит выбрать Rezer?</h2>
-        <ul>
-          <li>Полный цикл производства от А до Я</li>
-          <li>Продажа без посредников</li>
-          <li>Низкая цена</li>
-          <li>Быстрые поставки</li>
-        </ul>
-      </div>
-      <div className="video-block">
-        <h2>Наше производство</h2>
-        <div className="video" />
-      </div>
+  <div className="landing-info container-small">
+    <div className="why-us">
+      <h2>Почему стоит выбрать Rezer?</h2>
+      <ul>
+        <li>Полный цикл производства от А до Я</li>
+        <li>Продажа без посредников</li>
+        <li>Низкая цена</li>
+        <li>Быстрые поставки</li>
+      </ul>
+    </div>
+    <div className="video-block">
+      <h2>Наше производство</h2>
+      <div className="video" />
     </div>
   </div>
 );
