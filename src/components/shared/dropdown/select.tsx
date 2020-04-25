9322521dc0f466d304bf793ca795fcee2d9b92db
SashaGraves <60065704+SashaGraves@users.noreply.github.com>
2020-04-25 17:48:20 +0300

    Rzr-74-dropdown as <select> (#25)
    * RZR-74: Dropdown - initial implementation (as pure <select>)
    
    * RZR-74-minor-changes
    
    Co-authored-by: sashagraves <all-exa@ya.ru>

---
+++ src/components/shared/dropdown/select.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './select.scss';
+
+export default () => (
+  <select className="dropdown" name="dropdown-list">
+    <option value="" selected>variant 1</option>
+    <option>variant 2</option>
+    <option>variant 3</option>
+    <option>variant 4</option>
+    <option>variant 5</option>
+    <option>variant 6</option>
+    <option>variant 7</option>
+  </select>
+);
