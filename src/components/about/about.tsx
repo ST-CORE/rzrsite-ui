d21db8542654ee2745b9a3b53ebe1807840ea0e7
sashagraves <all-exa@ya.ru>
2020-05-08 19:19:38 +0300

    RZR-45-About-us: initial implementation

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { MediaMatcher, ProvideMediaMatchers } from 'react-media-match';
-import AboutUs from './about-us/about-us';
 import './about.scss';
+import AboutUs from './about-us/about-us';
 import AnyQuestions from '../shared/any-questions/any-questions';
+import AnyQuestionsMobile from '../shared/any-questions/any-questions-mobile';
 
 export default () => (
   <ProvideMediaMatchers>
     <MediaMatcher
-      mobile="render for mobile"
-      // tablet={"tablet"} // mobile will be rendered for a "skipped" tablet - "pick value to the left"
+      mobile={
+        (
+          <div className="about mobile">
+            <AboutUs />
+            <AnyQuestionsMobile />
+          </div>
+        )
+      }
       desktop={
         (
-          <div className="about container-small">
+          <div className="about desktop container-small">
             <AboutUs />
             <AnyQuestions />
           </div>
