a56b76b47e1e97abfad4291b2a6fdd6d6e4b9ec6
sashagraves <all-exa@ya.ru>
2020-05-13 01:34:14 +0300

    RZR-103-after review

--- src/components/shared/email/email.tsx
+++ src/components/shared/email/email.tsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
-import { ProvideMediaMatchers, MediaMatcher } from 'react-media-match';
-import './email-mobile.scss';
+import './email.scss';
 
 
 export default () => (
-  <ProvideMediaMatchers>
-    <MediaMatcher
-      mobile={<a className="email mobile" href="mailto:Rezer@mail.ru">Rezer@mail.ru</a>}
-      desktop={<span className="email desktop">Rezer@mail.ru</span>}
-    />
-  </ProvideMediaMatchers>
+  <a className="email mobile" href="mailto:Rezer@mail.ru">Rezer@mail.ru</a>
 );
