819aeffe2dfddd6007a1a703e3b904b43266917b
SashaGraves <60065704+SashaGraves@users.noreply.github.com>
2020-05-13 12:45:23 +0300

    RZR-79-Products improvements (#40)
    * RZR-79: Modal for big pictures
    
    * RZR-79-Products-improvement
    
    Co-authored-by: sashagraves <all-exa@ya.ru>

--- src/components/products/choose-model/product-pictures/product-pictures.tsx
+++ src/components/products/choose-model/product-pictures/product-pictures.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import './product-pictures.scss';
+import Modal from '../../../shared/modal/modal';
 
-import image1 from '../../../../images/products/boilers/small-boiler.png';
-import image2 from '../../../../images/products/boilers/medium-boiler.png';
+import imageSmallBoiler from '../../../../images/products/boilers/small-boiler.png';
+import imageMediumBoiler from '../../../../images/products/boilers/medium-boiler.png';
+import imageLargeBoiler from '../../../../images/products/boilers/boiler-large.png';
+import imageFit20Large from '../../../../images/products/boilers/Fit20-large.jpg';
 
 export default () => {
   const imgs = {
-    img1: image1,
-    img2: image1,
-    img3: image1,
-    img4: image2,
+    image1: {
+      imgSmall: imageSmallBoiler,
+      alt: 'Rezer',
+      imgBig: imageLargeBoiler,
+    },
+    image2: {
+      imgSmall: imageSmallBoiler,
+      alt: 'Rezer',
+      imgBig: imageLargeBoiler,
+    },
+    image3: {
+      imgSmall: imageSmallBoiler,
+      alt: 'Rezer',
+      imgBig: imageLargeBoiler,
+    },
+    image4: {
+      imgSmall: imageMediumBoiler,
+      alt: 'Rezer Fit-20',
+      imgBig: imageFit20Large,
+    },
+  };
+  
+  const [modal, openModal] = React.useState(false);
+  const [viewedImage, changeImage] = React.useState('');
+  const [viewedImageAlt, changeAlt] = React.useState('');
+  
+  const openPicture = (a: object) => {
+    changeImage(a.imgBig);
+    changeAlt(a.alt);
+    openModal(true);
   };
   
   return (
     <div className="product-pictures desktop">
-      <div className="small-picture"><img src={imgs.img1} alt="" /></div>
-      <div className="small-picture"><img src={imgs.img2} alt="" /></div>
-      <div className="small-picture"><img src={imgs.img3} alt="" /></div>
-      <div className="medium-picture"><img src={imgs.img4} alt="" /></div>
+      <div className="small-picture"><button type="button" onClick={() => openPicture(imgs.image1)}><img src={imgs.image1.imgSmall} alt={imgs.image1.alt} /></button></div>
+      <div className="small-picture"><button type="button" onClick={() => openPicture(imgs.image2)}><img src={imgs.image2.imgSmall} alt={imgs.image2.alt} /></button></div>
+      <div className="small-picture"><button type="button" onClick={() => openPicture(imgs.image3)}><img src={imgs.image3.imgSmall} alt={imgs.image3.alt} /></button></div>
+      <div className="medium-picture"><button type="button" onClick={() => openPicture(imgs.image4)}><img src={imgs.image4.imgSmall} alt={imgs.image4.alt} /></button></div>
+      
+      <Modal modalState={modal} closeModal={() => openModal(false)} overlay>
+        <img src={viewedImage} alt={viewedImageAlt} />
+      </Modal>
     </div>
   );
 };
