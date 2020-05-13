85546cc5c2318257b0eb9387e4ae6bf03c693d8a
sashagraves <all-exa@ya.ru>
2020-05-14 00:14:18 +0300

    RZR-92-Focus-Hover-etc

--- src/components/footer/nav-footer/footer-menu/footer-menu.tsx
+++ src/components/footer/nav-footer/footer-menu/footer-menu.tsx
@@ -1,28 +1,25 @@
 import * as React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './footer-menu.scss';
 
 export default () => (
   <div className="footer-menu">
     <p>
-      <span>
-        <Link to="/products">
-          Продукция
-        </Link>
-        :
-      </span>
-      <NavLink to="/products/boilers" activeClassName="selected"><span className="underline">Котлы</span></NavLink>
-      <NavLink to="/products/crushers" activeClassName="selected"><span className="underline">Дробилки</span></NavLink>
+      <NavLink to="/products" className="bold-link" activeClassName="bold-selected">
+        Продукция:
+      </NavLink>
+      
+      <NavLink to="/products/boilers" className="underlined-link" activeClassName="underlined-selected"><span className="underline">Котлы</span></NavLink>
+      <NavLink to="/products/crushers" className="underlined-link" activeClassName="underlined-selected"><span className="underline">Дробилки</span></NavLink>
     </p>
     <p>
-      <span>
-        <Link to="/about">
-          О компании
-        </Link>
-        :
-      </span>
-      <NavLink to="/delivery" activeClassName="selected"><span className="underline">Доставка</span></NavLink>
-      <NavLink to="/contacts" activeClassName="selected"><span className="underline">Контакты</span></NavLink>
+      
+      <NavLink to="/about" className="bold-link" activeClassName="bold-selected">
+        О компании:
+      </NavLink>
+      
+      <NavLink to="/delivery" className="underlined-link" activeClassName="underlined-selected"><span className="underline">Доставка</span></NavLink>
+      <NavLink to="/contacts" className="underlined-link" activeClassName="underlined-selected"><span className="underline">Контакты</span></NavLink>
     </p>
   </div>
 );
