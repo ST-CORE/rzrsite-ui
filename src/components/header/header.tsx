ec0fd7d3ee47b4c6007c9500d86219ff83183894
sashagraves <all-exa@ya.ru>
2020-05-16 14:19:23 +0300

    First test

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,28 @@
 import * as React from 'react';
 import { MediaMatcher, ProvideMediaMatchers } from 'react-media-match';
+import axios from 'axios';
 import PhoneHeader from './phone-header/phone-header';
 import NavHeader from './nav-header/nav-header';
 import HeaderMobile from './header-mobile/header-mobile';
+import apiUrl from '../../consts/api';
 
 interface HeaderProps {
   lightTheme: boolean
 }
 
-export default ({ lightTheme }: HeaderProps) => (
-  <ProvideMediaMatchers>
-    <MediaMatcher
-      mobile={<HeaderMobile lightTheme={lightTheme} />}
-      desktop={(
-        <header>
-          <PhoneHeader lightTheme={lightTheme} />
-          <NavHeader lightTheme={lightTheme} />
-        </header>
+export default ({ lightTheme }: HeaderProps) => {
+  axios.get(`${apiUrl}/category/`).then((response) => console.log(response));
+  return (
+    <ProvideMediaMatchers>
+      <MediaMatcher
+        mobile={<HeaderMobile lightTheme={lightTheme} />}
+        desktop={(
+          <header>
+            <PhoneHeader lightTheme={lightTheme} />
+            <NavHeader lightTheme={lightTheme} />
+          </header>
       )}
-    />
-  </ProvideMediaMatchers>
-);
+      />
+    </ProvideMediaMatchers>
+  );
+};
